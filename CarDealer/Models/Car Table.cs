bd49482ceee0dfee81d5fea7324a1c69c12666bb
Dustin Fox <djf149@hotmail.com>
2021-03-13 01:40:50 -0500

    Add project files.

---
+++ CarDealer/Models/Car Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public class Car_Table
+    {
+        public int Id { get; set; }
+         public virtual Dealer_table Dealer_Table { get; set; }
+        public int DealerId { get; set; }
+        public string Vin { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+        public int Year { get; set; }
+        public string Status { get; set; }
+        public decimal Cost { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal Profit { get; set; }
+    }
+}
---
+++ CarDealer/Models/Dealer table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Models
+{
+    public class Dealer_table
+    {
+        public int Id { get; set; }
+        [StringLength(30), Required]
+        public string Code { get; set; }
+        [StringLength(30), Required]
+        public string City { get; set; }
+        [StringLength(30), Required]
+        public string State { get; set; }
+        [StringLength(12)]
+        public string Phone { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        public int CarCount { get; set; }
+
+        public Dealer_table() { }
+
+    }
+}
